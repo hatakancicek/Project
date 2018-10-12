bb6035df50725fa5ff6135ee9894e92686b4f199
hakaneroztekin <eroztekin@itu.edu.tr>
2018-10-12 11:01:38 +0300

    adding input text (not completed)

---
+++ Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+
+    public Text answerText;
+    public InputField inputField;
+    private AnswerData answerData;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button button = gameObject.GetComponent<Button>();
+    }
+
+    void Awake()
+    {
+        inputField.onEndEdit.AddListener(AcceptStringInput);
+    }
+
+
+    public void Setup(AnswerData data)
+    {
+        answerData = data;
+        answerText.text = answerData.getAnswer();
+    }
+
+    void AcceptStringInput(string userInput)
+    {
+        userInput = userInput.ToLower();
+        controller.LogStringWithReturn(userInput);
+
+        char[] delimiterCharacters = { ' ' };
+        string[] separatedInputWords = userInput.Split(delimiterCharacters);
+
+        for (int i = 0; i < controller.inputActions.Length; i++)
+        {
+            InputAction inputAction = controller.inputActions[i];
+            if (inputAction.keyWord == separatedInputWords[0])
+            {
+                inputAction.RespondToInput(controller, separatedInputWords);
+            }
+        }
+
+        InputComplete();
+
+    }
+    void InputComplete()
+    {
+        controller.DisplayLoggedText();
+        inputField.ActivateInputField();
+        inputField.text = null;
+    }
+
+}
---
+++ Assets/Scripts/AnswerData.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class AnswerData : MonoBehaviour
+{
+    public string answer;
+    public string correctAnswer;
+
+    string _answer;
+    Text _text;
+
+    private void Awake()
+    {
+        _text = gameObject.GetComponent<Text>();
+        UpdateAnswer(answer);
+    }
+
+    public string getAnswer()
+    {
+        return _text.text;
+    }
+
+    void UpdateAnswer(string answer)
+    {
+
+        _answer = answer;
+        _text.text = answer;
+        //isCorrect(answer);
+    }
+
+    bool isCorrect(string answer) {
+        if (answer == correctAnswer)
+            // puan ver
+            return true;
+        else
+            // can eksilt
+            return false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+
+}
