8bb6d5b72049eab1638f79ff06370e27ad755deb
furkan1080 <qwaszxedc123456@gmail.com>
2018-10-14 20:10:19 +0300

    Add random level generator

--- Assets/Scripts/CheckSolution.cs
+++ Assets/Scripts/CheckSolution.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System;
-
 public class CheckSolution : MonoBehaviour
 {
     public GameObject Answer;
@@ -18,10 +17,15 @@ public class CheckSolution : MonoBehaviour
     public StarManager instance;
     public ScoreChange score;
     public ScoreChange questionNumber;
-    public bool isFirstFalse = true;
     public int SceneNumber;
     //static public CheckSolution GameObject;
     // Use this for initialization
+     private void Start()
+     {
+         Check();
+         Answer.GetComponent<Text>().enabled = false;
+
+     }
     public void Check()
     {
         int option1;
@@ -34,21 +38,10 @@ public class CheckSolution : MonoBehaviour
         int randomOption3=0;
         int randomOption4=0;
         int[] liste = {  randomNumber1,  randomNumber2,  randomOption1,  randomOption2, randomOption3, randomOption4 };
-        string trueAnswer = Answer.GetComponent<Text>().text;
-        Answer.GetComponent<Text>().enabled = true;
-        if (trueAnswer != GetComponent<Text>().text)
-        {
-            instance.LooseAStar();
-            isFirstFalse = false;
-            Answer.GetComponent<Text>().enabled = true;
-        }
-        else score.increaseScore();
-        questionNumber.increaseScore();
         int option = UnityEngine.Random.Range(0, 4);
         for(int i=0;i<6; i++)
         {
             int j ;
-            
             if (i == 0) liste[i] = UnityEngine.Random.Range(0, 40);
             else
             {
@@ -63,9 +56,6 @@ public class CheckSolution : MonoBehaviour
                 }
             }
             print(liste[i]);
-
-
-
         }
         Number1.GetComponent<Text>().text = liste[0].ToString();
         Number2.GetComponent<Text>().text = liste[1].ToString();
@@ -77,11 +67,23 @@ public class CheckSolution : MonoBehaviour
         int.TryParse(Second2.GetComponent<Text>().text, out option2);
         answer = option1 + option2;
         Answer.GetComponent<Text>().text = answer.ToString();
-        Answer.GetComponent<Text>().enabled = false;
         if (option == 0) Number1.GetComponent<Text>().text = Answer.GetComponent<Text>().text;
         else if (option == 1) Number2.GetComponent<Text>().text = Answer.GetComponent<Text>().text;
         else if (option == 2) Number3.GetComponent<Text>().text = Answer.GetComponent<Text>().text;
         else if (option == 3) Number4.GetComponent<Text>().text = Answer.GetComponent<Text>().text;
     }
-
+    public void CheckIfTrue()
+    {
+        int option1;
+        int option2;
+        int answer;
+        string trueAnswer = Answer.GetComponent<Text>().text;
+        if (trueAnswer != GetComponent<Text>().text) instance.LooseAStar();
+        else score.increaseScore();
+        questionNumber.increaseScore();
+        int.TryParse(First1.GetComponent<Text>().text, out option1);
+        int.TryParse(Second2.GetComponent<Text>().text, out option2);
+        answer = option1 + option2;
+        Answer.GetComponent<Text>().text = answer.ToString();
+    }
 }
